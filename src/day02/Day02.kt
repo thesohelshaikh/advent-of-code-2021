726b63b716ec89fefc59847e9d38b6f4cab8ac6c
Sohel Shaikh <sohelshaikh97@gmail.com>
2021-12-04 17:22:35 +0530

    Add Day02 part 2

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -10,24 +10,20 @@ object Day02 : Solution {
 
     data class Instruction(val direction: Direction, val steps: Int)
 
-    override fun part1(input: List<String>): Any {
+    private fun parseInstructions(input: List<String>): ArrayList<Instruction> {
         val instructions = ArrayList<Instruction>()
 
         input.forEach { command ->
-            val str = command.split(" ")
-            val directionStr = str.first()
-            val steps = str.last().toInt()
+            val (directionStr, steps) = command.split(" ")
 
             val direction = when (directionStr) {
                 "forward" -> Direction.FORWARD
                 "down" -> Direction.DOWN
                 else -> Direction.UP
             }
-            instructions.add(Instruction(direction, steps))
+            instructions.add(Instruction(direction, steps.toInt()))
         }
-
-        return getSubmarinePosition(instructions)
-
+        return instructions
     }
 
     private fun getSubmarinePosition(instructions: ArrayList<Instruction>): Int {
@@ -44,8 +40,38 @@ object Day02 : Solution {
         return x * y
     }
 
+    private fun getSubmarinePositionWithAim(instructions: ArrayList<Instruction>): Int {
+        var x = 0
+        var y = 0
+        var aim = 0
+
+        instructions.forEach { instruction ->
+            when (instruction.direction) {
+                Direction.FORWARD -> {
+                    x += instruction.steps
+                    if (aim != 0) {
+                        y += instruction.steps * aim
+                    }
+                }
+                Direction.UP -> aim -= instruction.steps
+                Direction.DOWN -> aim += instruction.steps
+            }
+        }
+
+        return x * y
+    }
+
+    override fun part1(input: List<String>): Any {
+        val instructions = parseInstructions(input)
+
+        return getSubmarinePosition(instructions)
+
+    }
+
     override fun part2(input: List<String>): Any {
-        return ""
+        val instructions = parseInstructions(input)
+
+        return getSubmarinePositionWithAim(instructions)
     }
 }
 
